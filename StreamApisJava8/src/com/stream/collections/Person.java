15bf46161f46ce1393a5b00bc5eeeea3bcb286e6
GaneshanRS <karthick2506@gmail.com>
2017-04-01 14:55:02 -0500

    Intial Commit - Stream Lamda and Foreach Java8

---
+++ StreamApisJava8/src/com/stream/collections/Person.java
@@ -0,0 +1,22 @@
+package com.stream.collections;
+
+public class Person {
+
+  String name;
+  int age;
+  
+  Person(String name,int age){
+	  this.name = name;
+	  this.age = age;
+  }
+  
+	public String getName() {
+		return name;
+	}
+	 
+	public int getAge() {
+		return age;
+	}
+	
+  
+}
